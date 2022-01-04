e6839e03bfe5cb6b4cb9f3bd46c35a4a47a7bfc7
Philippe SABAA <philippe.sabaa@insee.fr>
2022-01-04 12:00:25 +0100

    Mise en place base postgres embarquée pour les tests

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/config/AbstractTestIntegrationConfiguration.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/config/AbstractTestIntegrationConfiguration.java
@@ -2,13 +2,18 @@ package fr.insee.formation.hibernate.config;
 
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import fr.insee.formation.hibernate.Application;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase;
+import io.zonky.test.db.AutoConfigureEmbeddedDatabase.DatabaseProvider;
 
 /**
  * Classe abstraite dont doivent hériter toutes les classes de test
+ * d'intégration. Cette configuration inclu l'utilisation d'une base de donnée
+ * embarquée de type Postgres
  * 
  * @author SYV27O
  *
@@ -17,6 +22,7 @@ import fr.insee.formation.hibernate.Application;
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = { Application.class })
 @ActiveProfiles("test")
-public abstract class AbstractTestConfiguration {
-
+@Rollback
+@AutoConfigureEmbeddedDatabase(provider = DatabaseProvider.ZONKY)
+public abstract class AbstractTestIntegrationConfiguration {
 }
