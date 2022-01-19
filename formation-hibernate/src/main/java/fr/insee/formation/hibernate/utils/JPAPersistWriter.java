37c1e79e85150d1482e2b861036c3890bf929c31
Philippe SABAA <philippe.sabaa@insee.fr>
2022-01-19 12:46:39 +0100

    Ajout flush Hibernate pour éviter outofmemoryerror

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/utils/JPAPersistWriter.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/utils/JPAPersistWriter.java
@@ -7,6 +7,9 @@ import javax.persistence.EntityManager;
 import org.springframework.batch.item.ItemWriter;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 public class JPAPersistWriter<X> implements ItemWriter<X> {
 
 	@Autowired
@@ -18,6 +21,9 @@ public class JPAPersistWriter<X> implements ItemWriter<X> {
 		for (X item : items) {
 			entityManager.persist(item);
 		}
+
+		log.debug("{} items de type {} sont persistés", items.size(), items.get(0).getClass().getName());
+		entityManager.flush();
 
 	}
 
