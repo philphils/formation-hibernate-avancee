2d98289ecf08477505591e81c5a30369866a7415
Philippe SABAA <philippe.sabaa@insee.fr>
2022-01-19 12:48:04 +0100

    Test sur les dates JavaFaker

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/batch/JavaFakerTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/batch/JavaFakerTest.java
@@ -1,5 +1,9 @@
 package fr.insee.formation.hibernate.batch;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.util.Date;
 import java.util.Locale;
 
 import org.junit.Test;
@@ -15,11 +19,25 @@ public class JavaFakerTest extends AbstractTestConfiguration {
 	@Test
 	public void helloWorldTest() {
 
-		Faker feku = new Faker(new Locale("fr"));
+		Faker faker = new Faker(new Locale("fr"));
 
-		log.info(feku.address().fullAddress());
-		log.info(feku.address().fullAddress());
-		log.info(feku.address().fullAddress());
+		// Obtention de 3 adresses différentes
+		log.info(faker.address().fullAddress());
+		log.info(faker.address().fullAddress());
+		log.info(faker.address().fullAddress());
+
+		// Obtention d'une date aléatoire entre le 01/01/1970 et le 01/01/2022
+		Date dateDebutCreation = java.sql.Date.valueOf(LocalDate.of(1970, 01, 01));
+		Date dateFinCreation = java.sql.Date.valueOf(LocalDate.of(2022, 01, 01));
+
+		Date dateAleatoire = faker.date().between(dateDebutCreation, dateFinCreation);
+
+		DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+
+		log.info(dateFormat.format(dateAleatoire));
+
+		// Obtention de nom d'entreprise en français
+		log.info(faker.company().name());
 
 	}
 
