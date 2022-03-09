4bc40da47fdbc6e44d1ea44ad25954679be90481
Philippe SABAA <philippe.sabaa@insee.fr>
2022-03-09 17:35:14 +0100

    Nouvelles méthodes repository Declaration avec test correspondant

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/repositories/DeclarationRepository.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/repositories/DeclarationRepository.java
@@ -1,9 +1,25 @@
 package fr.insee.formation.hibernate.repositories;
 
+import java.util.Date;
+import java.util.Optional;
+import java.util.Set;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import fr.insee.formation.hibernate.model.Declaration;
+import fr.insee.formation.hibernate.model.Entreprise;
 
-public interface DeclarationRepository extends JpaRepository<Declaration, Integer>{
+public interface DeclarationRepository extends JpaRepository<Declaration, Integer> {
+
+	public Set<Declaration> findByEntreprise(Entreprise entreprise);
+
+	public Set<Declaration> findByEntrepriseAndDate(Entreprise entreprise, Date date);
+
+	//// @formatter:off
+	@Query("SELECT decl FROM Declaration decl "
+			+ "JOIN FETCH decl.entreprise entreprise "
+			+ "WHERE decl.id = :identifiant ")
+	public Optional<Declaration> findByIdWithEntreprise(Integer identifiant);
 
 }
--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/repositories/DeclarationRepositoryTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/repositories/DeclarationRepositoryTest.java
@@ -1,10 +1,17 @@
 package fr.insee.formation.hibernate.repositories;
 
+import static org.junit.Assert.assertEquals;
+
+import java.util.Set;
+
+import javax.persistence.EntityManager;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import fr.insee.formation.hibernate.config.AbstractTest;
+import fr.insee.formation.hibernate.model.Declaration;
 import fr.insee.formation.hibernate.util.JeuxTestUtil;
 import lombok.extern.slf4j.Slf4j;
 import net.ttddyy.dsproxy.QueryCountHolder;
@@ -17,6 +24,9 @@ public class DeclarationRepositoryTest extends AbstractTest {
 
 	@Autowired
 	private JeuxTestUtil jeuxTestUtil;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	private static boolean databaseIsInitialized = false;
 
@@ -41,6 +51,52 @@ public class DeclarationRepositoryTest extends AbstractTest {
 
 		log.info("Hello World Test is working !!!");
 
+	}
+
+	@Test
+	public void testFindByEntreprise() {
+
+		QueryCountHolder.clear();
+
+		Declaration declaration = declarationRepository.findAll().get(0);
+
+		Set<Declaration> declarations = declarationRepository.findByEntreprise(declaration.getEntreprise());
+
+		log.info("Nombre de déclarations de l'entreprise {} en base {}", declaration.getEntreprise().getDenomination(),
+				declarations.size());
+
+		Set<Declaration> declarations2 = declarationRepository.findByEntrepriseAndDate(declaration.getEntreprise(),
+				declaration.getDate());
+
+		assertEquals(1, declarations2.size());
+
+		assertEquals(declaration.getId(), declarations2.iterator().next().getId());
+
+		log.info("Nombre de requête SELECT exécutées : {}", QueryCountHolder.getGrandTotal().getSelect());
+
+	}
+
+	@Test
+	public void testFindDeclarationWithEntreprise() {
+
+		Declaration declaration = declarationRepository.findAll().get(0);
+
+		Integer id = declaration.getId();
+
+		entityManager.clear();
+
+		QueryCountHolder.clear();
+
+		Declaration declaration2 = declarationRepository.findByIdWithEntreprise(id).get();
+
+		String denom = declaration2.getEntreprise().getDenomination();
+
+		/*
+		 * On vérifie que la déclaration a bien été récupérée avec 2 requêtes Pourquoi
+		 * n'a-t-on pas une seule requête ? Proposer des amléiorations pour n'avoir
+		 * qu'une requête
+		 */
+		assertEquals(2, QueryCountHolder.getGrandTotal().getSelect());
 	}
 
 }
