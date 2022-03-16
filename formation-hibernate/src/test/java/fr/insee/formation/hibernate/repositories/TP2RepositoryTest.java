e0a5370ad595dbd40704b0e9eb7373623934f0b7
Philippe SABAA <philippe.sabaa@insee.fr>
2022-03-16 12:08:55 +0100

    Excercice 2

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/repositories/TP2RepositoryTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/repositories/TP2RepositoryTest.java
@@ -6,18 +6,31 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Optional;
 
+import javax.persistence.EntityManager;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import fr.insee.formation.hibernate.config.AbstractTest;
 import fr.insee.formation.hibernate.model.Entreprise;
+import fr.insee.formation.hibernate.model.Secteur;
 import fr.insee.formation.hibernate.util.JeuxTestUtil;
+import lombok.extern.slf4j.Slf4j;
+import net.ttddyy.dsproxy.QueryCountHolder;
 
+@Slf4j
 public class TP2RepositoryTest extends AbstractTest {
 
 	@Autowired
 	private JeuxTestUtil jeuxTestUtil;
+
+	@Autowired
+	private SecteurRepository secteurRepository;
+
+	@Autowired
+	private EntityManager entityManager;
 
 	private static boolean databaseIsInitialized = false;
 
@@ -75,6 +88,40 @@ public class TP2RepositoryTest extends AbstractTest {
 		// "N'importe quoi")
 
 		assertFalse(optionalEntreprise.isPresent());
+
+	}
+
+	@Test
+	@Transactional
+	public void testExercice2() {
+
+		Secteur secteur = secteurRepository.findAll().get(0);
+
+		Integer idSecteur = secteur.getId();
+
+		Entreprise entreprise = secteur.getEntreprises().iterator().next();
+
+		secteur = null;
+
+		entityManager.clear();
+
+		QueryCountHolder.clear();
+
+		// **********************************************************************************//
+		// Remplacer ici null par l'appel à une méthode que vous définirez dans
+		// SecteurRepository
+		// **********************************************************************************//
+		Secteur secteur2 = null;
+		// **********************************************************************************//
+
+		for (Entreprise entreprise2 : secteur2.getEntreprises()) {
+			log.info("L'Entreprise {} est dans le secteur {}", entreprise2.getDenomination(),
+					secteur2.getLibelleNomenclature());
+		}
+
+		assertTrue(secteur2.getEntreprises().stream().anyMatch(ent -> ent.getId() == entreprise.getId()));
+
+		assertEquals(1, QueryCountHolder.getGrandTotal().getSelect());
 
 	}
 
