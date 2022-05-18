34f0ba114ed8398b38f696374c246622d817939a
Philippe SABAA <philippe.sabaa@insee.fr>
2022-05-18 17:38:31 +0200

    Evolution du batch création données avec nouveau modèle nomenclature

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/batch/creationJeuDonnees/CreationSecteurProcessor.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/batch/creationJeuDonnees/CreationSecteurProcessor.java
@@ -21,6 +21,11 @@ import fr.insee.formation.hibernate.model.Declaration;
 import fr.insee.formation.hibernate.model.Entreprise;
 import fr.insee.formation.hibernate.model.TypeVoie;
 import fr.insee.formation.hibernate.model.nomenclature.AbstractNiveauNomenclature;
+import fr.insee.formation.hibernate.model.nomenclature.Classe;
+import fr.insee.formation.hibernate.model.nomenclature.Division;
+import fr.insee.formation.hibernate.model.nomenclature.Groupe;
+import fr.insee.formation.hibernate.model.nomenclature.Section;
+import fr.insee.formation.hibernate.model.nomenclature.SousClasse;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
@@ -40,6 +45,14 @@ public class CreationSecteurProcessor implements ItemProcessor<String[], Abstrac
 
 	private Integer compteurDeclarationsCreees = 1;
 
+	/*
+	 * On n'utilise qu'un objet Random pour des questions de performances
+	 */
+	private Random random = new Random();
+	private Faker faker = new Faker(new Locale("fr"));
+
+	private AbstractNiveauNomenclature precedentNiveauNomenclature = null;
+
 	LocalTime localTimeDebutJob = null;
 	LocalTime localTimeDebutBoucle = null;
 	LocalTime localTimeFinBoucle = null;
@@ -50,37 +63,149 @@ public class CreationSecteurProcessor implements ItemProcessor<String[], Abstrac
 		if (localTimeDebutJob == null)
 			localTimeDebutJob = LocalTime.now(ZoneId.of("Europe/Paris"));
 
-		AbstractNiveauNomenclature secteur = null; // TODO gérer la création des niveau aux différents échelons
+		String codeNaf = item[0];
+		String libelleNomenclature = item[1];
 
-		secteur.setCodeNaf(item[0]);
-		secteur.setLibelleNomenclature(item[1]);
-
-		Random random = new Random();
-
-		secteur.setCoeffRedressementNiveau(random.nextDouble());
-
-		Integer nbEntreprises = random.nextInt(dataScale);
-
-		Faker faker = new Faker(new Locale("fr"));
-
-		Date dateDebutCreation = java.sql.Date.valueOf(LocalDate.of(1970, 01, 01));
-		Date dateFinCreation = java.sql.Date.valueOf(LocalDate.of(2022, 01, 01));
+		AbstractNiveauNomenclature niveauNomenclature = creerNiveauNomenclature(codeNaf, libelleNomenclature);
 
 		/*
-		 * Création des entreprises
+		 * Si c'est un niveau SousClasse on créé les Entreprises
 		 */
-		Entreprise entreprise = null;
-		Address fakerAddress = null;
-		for (int i = 0; i < nbEntreprises; i++) {
+		if (niveauNomenclature instanceof SousClasse) {
 
-			creerEntreprise(random, faker, dateDebutCreation, dateFinCreation);
+			Integer nbEntreprises = random.nextInt(dataScale);
+
+			Date dateDebutCreation = java.sql.Date.valueOf(LocalDate.of(1970, 01, 01));
+			Date dateFinCreation = java.sql.Date.valueOf(LocalDate.of(2022, 01, 01));
+			/*
+			 * Création des entreprises
+			 */
+			Entreprise entreprise = null;
+			Address fakerAddress = null;
+			for (int i = 0; i < nbEntreprises; i++) {
+
+				((SousClasse) niveauNomenclature).addEntreprise(creerEntreprise(dateDebutCreation, dateFinCreation));
+
+			}
 
 		}
 
-		return secteur;
+		precedentNiveauNomenclature = niveauNomenclature;
+
+		return niveauNomenclature;
 	}
 
-	private void creerEntreprise(Random random, Faker faker, Date dateDebutCreation, Date dateFinCreation) {
+	private AbstractNiveauNomenclature creerNiveauNomenclature(String codeNaf, String libelleNomenclature) {
+
+		AbstractNiveauNomenclature niveauNomenclature = null;
+
+		/*
+		 * si precedentNiveauNomenclature est null, on est sur la premiere ligne du
+		 * fichier
+		 */
+		if (precedentNiveauNomenclature == null) {
+			/*
+			 * le premier Niveau de nomenclature doit être de type "section"
+			 */
+			if (!codeNaf.contains("SECTION"))
+				throw new RuntimeException("Le premier niveau doit être de type SECTION");
+			else {
+				niveauNomenclature = new Section();
+			}
+		} else {
+			if (codeNaf.contains("SECTION"))
+				niveauNomenclature = new Section();
+			else if (codeNaf.length() <= 2) {
+				niveauNomenclature = new Division();
+				/*
+				 * On calcule la Section à laquelle rattacher cette Division
+				 */
+				Section section = null;
+				if (precedentNiveauNomenclature instanceof Section)
+					section = ((Section) precedentNiveauNomenclature);
+				else if (precedentNiveauNomenclature instanceof SousClasse)
+					section = ((SousClasse) precedentNiveauNomenclature).getClasse().getGroupe().getDivision()
+							.getSection();
+				else
+					throwHierarchieNomenclatureException(codeNaf);
+				/*
+				 * Et on rattache la nouvelle division à cette Section
+				 */
+				section.addDivision((Division) niveauNomenclature);
+
+			} else if (codeNaf.length() <= 4) {
+				niveauNomenclature = new Groupe();
+				/*
+				 * On calcule la Division à laquelle rattacher ce Groupe
+				 */
+				Division division = null;
+				if (precedentNiveauNomenclature instanceof Division)
+					division = (Division) precedentNiveauNomenclature;
+				else if (precedentNiveauNomenclature instanceof SousClasse)
+					division = ((SousClasse) precedentNiveauNomenclature).getClasse().getGroupe().getDivision();
+				else
+					throwHierarchieNomenclatureException(codeNaf);
+
+				/*
+				 * Et on rattache ce nouveau Groupe à la Division
+				 */
+				division.addGroupe((Groupe) niveauNomenclature);
+
+			} else if (codeNaf.length() <= 5) {
+				niveauNomenclature = new Classe();
+				/*
+				 * On calcule le Groupe auquel rattacher cette Classe
+				 */
+				Groupe groupe = null;
+				if (precedentNiveauNomenclature instanceof Groupe)
+					groupe = (Groupe) precedentNiveauNomenclature;
+				else if (precedentNiveauNomenclature instanceof SousClasse)
+					groupe = ((SousClasse) precedentNiveauNomenclature).getClasse().getGroupe();
+				else
+					throwHierarchieNomenclatureException(codeNaf);
+
+				/*
+				 * Et on rattache cette nouvelle Classe à ce Groupe
+				 */
+				groupe.addClasse((Classe) niveauNomenclature);
+
+			} else if (codeNaf.length() <= 6) {
+				niveauNomenclature = new SousClasse();
+				/*
+				 * On calcule la Classe à laquelle rattacher cette SousClasse
+				 */
+				Classe classe = null;
+				if (precedentNiveauNomenclature instanceof Classe)
+					classe = (Classe) precedentNiveauNomenclature;
+				else if (precedentNiveauNomenclature instanceof SousClasse)
+					classe = ((SousClasse) precedentNiveauNomenclature).getClasse();
+				else
+					throwHierarchieNomenclatureException(codeNaf);
+
+				/*
+				 * Et on rattache cette nouvelle SousClasse à cette Classe
+				 */
+				classe.addSousClasse((SousClasse) niveauNomenclature);
+			}
+		}
+
+		niveauNomenclature.setCodeNaf(codeNaf);
+		niveauNomenclature.setLibelleNomenclature(libelleNomenclature);
+
+		niveauNomenclature.setCoeffRedressementNiveau(random.nextDouble());
+
+		niveauNomenclature.setCoeffCalculIndice(random.nextDouble());
+
+		return niveauNomenclature;
+
+	}
+
+	private void throwHierarchieNomenclatureException(String codeNaf) {
+		throw new RuntimeException("Le fichier ne respecte pas la hiérarchie des nomenclatures pour le niveau codeNaf :"
+				+ codeNaf + ". Le niveau précédant est de codeNaf : " + precedentNiveauNomenclature.getCodeNaf());
+	}
+
+	private Entreprise creerEntreprise(Date dateDebutCreation, Date dateFinCreation) {
 		Entreprise entreprise;
 		Address fakerAddress;
 		entreprise = new Entreprise();
@@ -131,16 +256,19 @@ public class CreationSecteurProcessor implements ItemProcessor<String[], Abstrac
 
 		for (int nbMois = 0; nbMois < nbMoisHistorique; nbMois++) {
 
-			creerDeclaration(random, entreprise, montantMoyen, nbMois);
+			entreprise.addDeclaration(creerDeclaration(montantMoyen, nbMois));
 
 			comptageDeclarationsCreees();
 
 			compteurDeclarationsCreees++;
 
 		}
+
+		return entreprise;
+
 	}
 
-	private void creerDeclaration(Random random, Entreprise entreprise, Integer montantMoyen, int nbMois) {
+	private Declaration creerDeclaration(Integer montantMoyen, int nbMois) {
 		Declaration declaration;
 		Date dateDeclaration;
 		Double montant;
@@ -153,7 +281,7 @@ public class CreationSecteurProcessor implements ItemProcessor<String[], Abstrac
 		declaration.setDate(dateDeclaration);
 		declaration.setMontant(montant);
 
-		entreprise.addDeclaration(declaration);
+		return declaration;
 	}
 
 	private void comptageDeclarationsCreees() {
