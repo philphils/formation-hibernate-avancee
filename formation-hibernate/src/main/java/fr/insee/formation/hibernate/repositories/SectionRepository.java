e2c335318debdda743451e06d31ecaf38775a4be
Philippe SABAA <philippe.sabaa@insee.fr>
2022-07-26 16:25:22 +0200

    Query cache

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/repositories/SectionRepository.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/repositories/SectionRepository.java
@@ -1,13 +1,21 @@
 package fr.insee.formation.hibernate.repositories;
 
+import java.util.List;
 import java.util.Optional;
 
+import javax.persistence.QueryHint;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.QueryHints;
 
 import fr.insee.formation.hibernate.model.nomenclature.Section;
 
 public interface SectionRepository extends JpaRepository<Section, Integer> {
 
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_CACHEABLE, value = "true"))
 	public Optional<Section> findByCodeNaf(String codeNaf);
+
+	@QueryHints(@QueryHint(name = org.hibernate.jpa.QueryHints.HINT_CACHEABLE, value = "true"))
+	public List<Section> findAll();
 
 }
