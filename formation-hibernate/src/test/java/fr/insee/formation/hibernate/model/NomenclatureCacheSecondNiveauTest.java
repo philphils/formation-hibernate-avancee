be80fa0ad92f35eda528987fce799a9b21747608
Philippe SABAA <philippe.sabaa@insee.fr>
2022-07-26 10:59:18 +0200

    Modif test et ajout test en false pour association section - division

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/model/NomenclatureCacheSecondNiveauTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/model/NomenclatureCacheSecondNiveauTest.java
@@ -1,15 +1,20 @@
 package fr.insee.formation.hibernate.model;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
+import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
+import javax.transaction.Transactional;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.transaction.TestTransaction;
 
 import fr.insee.formation.hibernate.config.AbstractTest;
 import fr.insee.formation.hibernate.model.nomenclature.Section;
@@ -27,6 +32,9 @@ public class NomenclatureCacheSecondNiveauTest extends AbstractTest {
 	@Autowired
 	private SectionRepository sectionRepository;
 
+	@PersistenceContext
+	EntityManager entityManager;
+
 	@PersistenceUnit
 	EntityManagerFactory entityManagerFactory;
 
@@ -39,16 +47,52 @@ public class NomenclatureCacheSecondNiveauTest extends AbstractTest {
 			databaseIsInitialized = true;
 		}
 
+		ouvrirNouvelleTransaction();
+
 		QueryCountHolder.clear();
 
 	}
 
 	@Test
+	@Transactional
 	public void testSectionCache() {
 
 		List<Section> sections = sectionRepository.findAll();
 
 		assertTrue(entityManagerFactory.getCache().contains(Section.class, 1));
+
+	}
+
+	@Test
+	@Transactional
+	public void testAssociationSectionDivisionCache() {
+
+		List<Section> sections = sectionRepository.findAll();
+
+		sections.forEach(section -> section.getDivisions()
+				.forEach(div -> log.info("Section : " + section.getCodeNaf() + ", div : " + div.getCodeNaf())));
+
+		TestTransaction.flagForCommit();
+		TestTransaction.end();
+
+		sections = null;
+
+		entityManager.clear();
+
+		QueryCountHolder.clear();
+
+		TestTransaction.start();
+
+		List<Section> sections2 = sectionRepository.findAll();
+
+		sections2.forEach(section -> section.getDivisions()
+				.forEach(div -> log.info("Section : " + section.getCodeNaf() + ", div : " + div.getCodeNaf())));
+
+		/*
+		 * Les requêtes de récupération des divisions ne doivent pas avoir été faîtes
+		 * car l'association doit avoir été mise en cache
+		 */
+		assertEquals(QueryCountHolder.getGrandTotal().getTotal(), 1);
 
 	}
 
