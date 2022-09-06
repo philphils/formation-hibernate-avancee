a942a9dfc54cc38cf5784f3260ec720216f25ff0
Philippe SABAA <philippe.sabaa@insee.fr>
2022-09-06 13:39:35 +0200

    Test vérification L2C

--- formation-hibernate/src/test/java/fr/insee/formation/hibernate/model/NomenclatureCacheSecondNiveauTest.java
+++ formation-hibernate/src/test/java/fr/insee/formation/hibernate/model/NomenclatureCacheSecondNiveauTest.java
@@ -18,8 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.transaction.TestTransaction;
 
 import fr.insee.formation.hibernate.config.AbstractTest;
+import fr.insee.formation.hibernate.model.nomenclature.Classe;
+import fr.insee.formation.hibernate.model.nomenclature.Division;
+import fr.insee.formation.hibernate.model.nomenclature.Groupe;
 import fr.insee.formation.hibernate.model.nomenclature.Section;
+import fr.insee.formation.hibernate.model.nomenclature.SousClasse;
 import fr.insee.formation.hibernate.repositories.SectionRepository;
+import fr.insee.formation.hibernate.repositories.SousClasseRepository;
 import fr.insee.formation.hibernate.util.JeuxTestUtil;
 import lombok.extern.slf4j.Slf4j;
 import net.ttddyy.dsproxy.QueryCountHolder;
@@ -32,6 +37,9 @@ public class NomenclatureCacheSecondNiveauTest extends AbstractTest {
 
 	@Autowired
 	private SectionRepository sectionRepository;
+
+	@Autowired
+	private SousClasseRepository sousClasseRepository;
 
 	@PersistenceContext
 	EntityManager entityManager;
@@ -49,6 +57,9 @@ public class NomenclatureCacheSecondNiveauTest extends AbstractTest {
 		}
 
 		ouvrirNouvelleTransaction();
+
+		entityManager.clear();
+		entityManagerFactory.getCache().evictAll();
 
 		QueryCountHolder.clear();
 
@@ -140,6 +151,128 @@ public class NomenclatureCacheSecondNiveauTest extends AbstractTest {
 		List<Section> sections2 = sectionRepository.findAll();
 
 		assertEquals(QueryCountHolder.getGrandTotal().getTotal(), 0);
+
+	}
+
+	@Test
+	@Transactional
+	public void testCacheAssociationSensDescendant() {
+
+		/*
+		 * Premier passage pour mise en cache
+		 */
+
+		Optional<Section> optionalSection = sectionRepository.findByCodeNaf("section1");
+
+		Section section = optionalSection.get();
+
+		for (Division division : section.getDivisions()) {
+
+			log.info("Division " + division.getCodeNaf());
+
+			for (Groupe groupe : division.getGroupes()) {
+
+				log.info("Groupe " + groupe.getCodeNaf());
+
+				for (Classe classe : groupe.getClasses()) {
+
+					log.info("Classe " + classe.getCodeNaf());
+
+					for (SousClasse sousClasse : classe.getSousClasses()) {
+
+						log.info("Sous Classe " + sousClasse.getCodeNaf());
+
+					}
+
+				}
+
+			}
+		}
+
+		entityManager.clear();
+
+		QueryCountHolder.clear();
+
+		/*
+		 * Deuxièmement passage pour vérification requêtes générées
+		 */
+
+		Optional<Section> optionalSection2 = sectionRepository.findByCodeNaf("section1");
+
+		Section section2 = optionalSection2.get();
+
+		for (Division division : section2.getDivisions()) {
+
+			log.info("Division " + division.getCodeNaf());
+
+			for (Groupe groupe : division.getGroupes()) {
+
+				log.info("Groupe " + groupe.getCodeNaf());
+
+				for (Classe classe : groupe.getClasses()) {
+
+					log.info("Classe " + classe.getCodeNaf());
+
+					for (SousClasse sousClasse : classe.getSousClasses()) {
+
+						log.info("Sous Classe " + sousClasse.getCodeNaf());
+
+					}
+
+				}
+
+			}
+		}
+
+		assertEquals(0, QueryCountHolder.getGrandTotal().getSelect());
+
+	}
+
+	@Test
+	@Transactional
+	public void testCacheAssociationSensAscendant() {
+
+		/*
+		 * Premier passage pour mise en cache
+		 */
+
+		Optional<SousClasse> optionalSousClasse = sousClasseRepository.findByCodeNaf("sousclasse5");
+
+		SousClasse sousClasse = optionalSousClasse.get();
+
+		log.info("Sous Classe " + sousClasse.getCodeNaf());
+
+		log.info("Classe " + sousClasse.getClasse().getCodeNaf());
+
+		log.info("Groupe " + sousClasse.getClasse().getGroupe().getCodeNaf());
+
+		log.info("Division " + sousClasse.getClasse().getGroupe().getDivision().getCodeNaf());
+
+		log.info("Section " + sousClasse.getClasse().getGroupe().getDivision().getSection().getCodeNaf());
+
+		entityManager.clear();
+
+		QueryCountHolder.clear();
+
+		/*
+		 * Deuxièmement passage pour vérification requêtes générées
+		 */
+
+		Optional<SousClasse> optionalSousClasse2 = sousClasseRepository.findByCodeNaf("sousclasse5");
+
+		SousClasse sousClasse2 = optionalSousClasse.get();
+
+		log.info("Sous Classe " + sousClasse2.getCodeNaf());
+
+		log.info("Classe " + sousClasse2.getClasse().getCodeNaf());
+
+		log.info("Groupe " + sousClasse2.getClasse().getGroupe().getCodeNaf());
+
+		log.info("Division " + sousClasse2.getClasse().getGroupe().getDivision().getCodeNaf());
+
+		log.info("Section " + sousClasse2.getClasse().getGroupe().getDivision().getSection().getCodeNaf());
+
+		assertEquals(0, QueryCountHolder.getGrandTotal().getSelect());
 
 	}
 
