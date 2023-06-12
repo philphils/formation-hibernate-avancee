2ad60942ca3c0d8c92740efd46e2515abb55e820
Philippe SABAA <philippe.sabaa@insee.fr>
2023-06-12 09:37:50 +0200

    Correction TP2

---
+++ Documentation/Corrections/Corrections TP2/DeclarationRepository.java
@@ -0,0 +1,56 @@
+package fr.insee.formation.hibernate.repositories;
+
+import java.util.Date;
+import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import fr.insee.formation.hibernate.model.Declaration;
+import fr.insee.formation.hibernate.model.Entreprise;
+import fr.insee.formation.hibernate.model.nomenclature.SousClasse;
+
+public interface DeclarationRepository extends JpaRepository<Declaration, Integer> {
+
+	public Set<Declaration> findByEntreprise(Entreprise entreprise);
+
+	public Set<Declaration> findByEntrepriseAndDate(Entreprise entreprise, Date date);
+
+	//// @formatter:off
+	@Query("SELECT decl FROM Declaration decl "
+			+ "JOIN FETCH decl.entreprise entreprise "
+			+ "WHERE decl.id = :identifiant ")
+	// @formatter:on
+	public Optional<Declaration> findByIdWithEntreprise(Integer identifiant);
+
+	public Page<Declaration> findByEntreprise(Entreprise entreprise, Pageable pageable);
+
+	@Query("SELECT decl FROM Declaration decl")
+	public Stream<Declaration> findAllStream();
+
+	/**
+	 * TP2 - Exercice 3 : Remplacer la requête ci-dessous par une requête permettant
+	 * de récupérer les déclarations avec leur entreprises et leur
+	 * {@link SousClasse} associés
+	 * 
+	 * @return
+	 */
+	//// @formatter:off
+	@Query("SELECT declaration FROM Declaration declaration "
+			+ "JOIN FETCH declaration.entreprise entreprise "
+			+ "JOIN FETCH entreprise.sousClasse sousClasse")
+	// @formatter:on
+	public Set<Declaration> findAllDeclarationWithEntrepriseWithSousClasse();
+
+	//// @formatter:off
+	@Query("SELECT declaration FROM Declaration declaration "
+			+ "JOIN FETCH declaration.entreprise entreprise "
+			+ "JOIN FETCH entreprise.sousClasse sousClasse")
+	// @formatter:on
+	public Stream<Declaration> streamAllDeclarationWithEntrepriseWithSousClasse();
+
+}
---
+++ Documentation/Corrections/Corrections TP2/SousClasseRepository.java
@@ -0,0 +1,45 @@
+package fr.insee.formation.hibernate.repositories;
+
+import java.util.Optional;
+import java.util.Set;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import fr.insee.formation.hibernate.model.Entreprise;
+import fr.insee.formation.hibernate.model.nomenclature.SousClasse;
+
+public interface SousClasseRepository extends JpaRepository<SousClasse, Integer> {
+
+	public Optional<SousClasse> findByCodeNaf(String codeNaf);
+
+	//// @formatter:off
+	@Query(value = "SELECT ssClasse FROM SousClasse ssClasse "
+			+ " LEFT JOIN FETCH ssClasse.entreprises ent")
+	// @formatter:on
+	public Set<SousClasse> findAllWithEntreprises();
+
+	//// @formatter:off
+	@Query(value = "SELECT ssClasse FROM SousClasse ssClasse "
+			+ " LEFT JOIN FETCH ssClasse.indicesMensuels indMens "
+			+ " LEFT JOIN FETCH ssClasse.indicesAnnuels indAnn ")
+	// @formatter:on
+	public Set<SousClasse> findAllWithIndicesMensuelsAndAnnuels();
+
+	/**
+	 * TP2 : Exercice 2 : Modifier la requête pour récupérer un {@link SousClasse}
+	 * ayant une {@link Entreprise} donnée en paramètre, avec toutes ses
+	 * {@link Entreprise}
+	 * 
+	 * @param entreprise
+	 * @return
+	 */
+	//// @formatter:off
+	@Query("SELECT sousClasse FROM SousClasse sousClasse "
+			+ "JOIN sousClasse.entreprises entreprise "
+			+ "JOIN FETCH sousClasse.entreprises entreprise2 "
+			+ "WHERE entreprise = :entreprise ")
+	// @formatter:on
+	public SousClasse findByEntrepriseWithAllEntreprises(Entreprise entreprise);
+
+}
