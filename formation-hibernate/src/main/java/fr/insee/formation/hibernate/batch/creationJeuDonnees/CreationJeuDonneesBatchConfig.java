93ecd9ee57703db6731e214f8f115a8d39c97ddd
Philippe Sabaa <philippe.sabaa@insee.fr>
2025-07-04 18:18:17 +0200

    fix : remove old imports unused

--- formation-hibernate/src/main/java/fr/insee/formation/hibernate/batch/creationJeuDonnees/CreationJeuDonneesBatchConfig.java
+++ formation-hibernate/src/main/java/fr/insee/formation/hibernate/batch/creationJeuDonnees/CreationJeuDonneesBatchConfig.java
@@ -4,8 +4,6 @@ import java.util.Set;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
-import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
 import org.springframework.batch.core.step.builder.StepBuilder;
